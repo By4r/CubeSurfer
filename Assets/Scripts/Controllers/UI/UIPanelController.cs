510beb874a153684a386cf00532656cf7db7cd74
By4r <j.alfarno142@gmail.com>
2023-03-10 15:06:07 +0300

    UI Root  Added

---
+++ Assets/Scripts/Controllers/UI/UIPanelController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using Enums;
+using Signals;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Controllers.UI
+{
+    public class UIPanelController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        
+
+        #endregion
+
+        #endregion
+    }
+}
