06ddda4ca5e357f17874cc49edc9ca8ea826dfdf
By4r <j.alfarno142@gmail.com>
2023-03-11 16:52:46 +0300

    UI Event Subscriber

--- Assets/Scripts/Controllers/UI/LevelPanelController.cs
+++ Assets/Scripts/Controllers/UI/LevelPanelController.cs
@@ -1,7 +1,54 @@
+using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using Signals;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+
 namespace Controllers.UI
 {
-    public class LevelPanelController
+    public class LevelPanelController : MonoBehaviour
     {
-        
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private List<TextMeshProUGUI> levelTexts = new List<TextMeshProUGUI>();
+        [Space] [SerializeField] private List<Image> stageImages = new List<Image>();
+
+        #endregion
+
+        #endregion
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            UISignals.Instance.onSetNewLevelValue += OnSetNewLevelValue;
+            //Instance.onSetStageColor += OnSetStageColor;
+        }
+
+        private void OnSetNewLevelValue(int levelValue)
+        {
+
+
+
+        }
+
+        private void UnSubscribeEvents()
+        {
+            UISignals.Instance.onSetNewLevelValue -= OnSetNewLevelValue;
+            //UISignals.Instance.onSetStageColor -= OnSetStageColor;
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
     }
 }
