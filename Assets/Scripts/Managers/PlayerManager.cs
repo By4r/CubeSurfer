4e4acaca5056aebbe5c2748e124a5256c962445d
By4r <j.alfarno142@gmail.com>
2023-03-22 11:32:01 +0300

    PlayerMovementController

---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,23 @@
+using Controllers.Player;
+using Data.UnityObjects;
+using Data.ValueObjects;
+using Signals;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+
+namespace Managers
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Public Variables
+
+        
+
+        #endregion
+
+        #endregion
+    }
+}
