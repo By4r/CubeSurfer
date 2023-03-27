d61187779c8e0278fb9a5a6f488e9b62a78a4acc
By4r <j.alfarno142@gmail.com>
2023-03-27 10:51:56 +0300

    Player Physics Controller

--- Assets/Scripts/Controllers/Player/PlayerPhysicsController.cs
+++ Assets/Scripts/Controllers/Player/PlayerPhysicsController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Signals;
 using Managers;
@@ -19,7 +20,12 @@ namespace Controllers.Player
 
         #region Private Variables
 
-        [ShowInInspector] private bool _isOver;
+        [ShowInInspector] private bool inCollision;
+
+        [ShowInInspector] private float speed;
+
+        [ShowInInspector] private GameObject lastCollision = null;
+        [ShowInInspector] private Transform characterTransform;
 
         #endregion
 
@@ -28,19 +34,65 @@ namespace Controllers.Player
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag("StageArea"))
+            if (other.CompareTag("Pickup") && !inCollision)
             {
-                InputSignals.Instance.onDisableInput?.Invoke();
+                if (other.gameObject != lastCollision)
+                {
+                    // Toplanan küpü karakterin üzerine taşıyın ve oyuncunun yüksekliğini ayarlayın
+                    other.transform.position = new Vector3(transform.position.x, other.transform.position.y,
+                        transform.position.z);
+                    transform.position += (Vector3.up * (other.transform.localScale.y / 2));
+                    characterTransform.position += Vector3.up * 0.25f;
+                    other.transform.parent = transform;
+                    lastCollision = other.gameObject;
 
+                    // Toplama işlemi tamamlandığında InputSignals'i devre dışı bırakın
+                    InputSignals.Instance.onDisableInput?.Invoke();
+                }
 
                 return;
             }
-            else CoreGameSignals.Instance.onLevelFailed?.Invoke();
+            else if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Obstacle"))
+            {
+                // Engellere veya duvarlara çarpıldığında
+                var thisCollider = other.contacts[0].thisCollider.transform;
 
+                // Yere düşmeden önce son toplanan küpü serbest bırakın
+                if (lastCollision != null)
+                {
+                    lastCollision.transform.parent = null;
+                }
 
+                inCollision = true;
+
+                // Küp sayısı 3'ten azsa karakter kaybeder
+                if (transform.childCount < 3)
+                {
+                    CoreGameSignals.Instance.onLevelFailed?.Invoke();
+                }
+                else
+                {
+                    // Küp sayısı 3 veya daha fazlaysa küp sayısı kadar küçülmeli
+                    var shrinkAmount = transform.child;
+                    Count / 4.0f;
+                    var newScale = transform.localScale - new Vector3(shrinkAmount, shrinkAmount, shrinkAmount);
+                    transform.DOScale(newScale, 0.5f);
+                    // Engellere çarptığında kaybetme animasyonunu oynatın
+                    if (other.gameObject.CompareTag("Wall"))
+                    {
+                        CoreGameSignals.Instance.onLevelFailed?.Invoke();
+                    }
+                    else // Obstacle
+                    {
+                        CoreGameSignals.Instance.onLevelSuccessful();
+                    }
+                }
+            }
         }
+
         private void OnDrawGizmos()
         {
+            // Hedef toplama noktasının yeri için bir gizmo çiz
             Gizmos.color = Color.yellow;
             var transform1 = manager.transform;
             var position = transform1.position;
@@ -49,6 +101,13 @@ namespace Controllers.Player
 
         public void OnReset()
         {
+            // Oyun sıfırlandığında tüm çocuk nesneleri yok edin ve boyutunu sıfırlayın
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+            transform.localScale = Vector3.one;
+            lastCollision = null;
+            inCollision = false;
         }
     }
-}
